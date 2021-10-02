faa1fb1c6e1d6d52cbb07a9933e0387e977b2e58
JD <jdmejia712@gmail.com>
2021-10-02 15:30:57 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
+console.log(multiply)
+
+let random = Math.floor((Math.random() * 10) +1);
 
 // Write your code here
+
+let num3 = 40
+let num4 = 6
+let mod = 40%6
+
+let max = Math.max(20, 4,6,20,5,8)
